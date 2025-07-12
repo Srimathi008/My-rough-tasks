e129b23ccf7147a3a929905c4aabd937c3258dc6
Srimathi008 <srimathimageswaran@gmail.com>
2025-07-12 10:48:46 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,30 @@
-const number=12
+//task 1
+const num=12
 var nam=`js`
 var iscloud=true
-console.log(number+" "+nam+ " "+ iscloud)
-document.writeln(number)
+console.log(num+" "+nam+ " "+ iscloud)
+document.writeln(num)
 document.write(nam)
 document.writeln(iscloud)
+
+
+//task 2
+console.log(typeof(num));
+console.log(typeof(nam));
+console.log(typeof(iscloud));
+
+
+//task 3
+var a=parseInt(prompt("first value"))
+var b=parseInt(prompt("second value"))
+var add=a+b
+var sub=a-b
+var mul=a*b
+var div=a/b
+var mod=a%b
+(console.log(add))
+console.log(sub);
+console.log(mul);
+console.log(div);
+console.log(mod);
+
