38177e3bb06c978043b98ea38526eb1b75d8a3af
Srimathi008 <srimathimageswaran@gmail.com>
2025-07-24 15:35:44 +0530

    completd

---
+++ day 4.js
@@ -0,0 +1,48 @@
+/let number=[1,2,"three",true];
+
+//let names=new Array("john","james");
+
+//console.log(number[2]);
+//console.log(names.length);
+//let result=number.filter(result=>number[1]);
+//console.log(result);
+//let number=[1,2,3,4,5];
+//number.splice(1,2,"hai");
+//console.log(number);
+//object
+/*let details={
+    name:"js",
+    totalmark:"90",
+    //nested obj
+    subject:{
+        java:"50",
+        ds:"80",
+    },
+    avg:function(){
+        let sum=parseInt(this.subject.ds)+parseInt(this.subject.java);
+        return sum;
+    }
+
+}
+console.log(details.totalmark);
+console.log(details["name"]);
+console.log(details.subject.java);
+console.log(details.subject["ds"]);
+console.log(details.avg());
+
+
+/*const person={
+  //  name:"sri",
+    //age:18,
+    //city:"chennai"
+};
+const{name,age,city}=person;
+console.log(name);
+console.log(age);
+console.log(city);*/
+
+//array destructuring
+const person={
+    "john"
+}
+
